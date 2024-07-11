843f62c713a1b2f26cb8673ff73872fe94f0e7c0
XiaoQuark <kris.oldrini@gmail.com>
2024-07-11 11:46:40 +0100

    fix database user profile error

--- autoloads/database_manager.gd
+++ autoloads/database_manager.gd
@@ -19,14 +19,12 @@ func create_empty_player_document(user_id: String) -> void:
 		"username": "",
 		"avatar_id": 0,
 		"avatar_img": "",
-		"score": 0  # Assuming score is an integer
+		"score": 0
 	}  # Empty dictionary for creating an empty document
 	var document = await firestore_collection.add(user_id, data)
-	print(document)
 	
 	if document:
 		print("Empty document created for user:", user_id)
-		print(document)
 	else:
 		print("Failed to create document for user:", user_id)
 
@@ -34,39 +32,35 @@ func save_score(user_id: String, score: int) -> void:
 	var firestore_collection: FirestoreCollection = Firebase.Firestore.collection(COLLECTION_NAME_PLAYERS)
 	var document = await firestore_collection.get_doc(user_id)
 	if document and not document.is_null_value("score"):
-		print("Updating existing score")
 		document.add_or_update_field("score", score)
 		await firestore_collection.update(document)
 	else:
-		print("Creating new document with score")
 		var data: Dictionary = {
 			"score": score
 		}
 		document = await firestore_collection.add(user_id, data)
-		print("Document created with ID: ", document.doc_name)
 		
 func update_player_data(user_id: String, username: String, avatar_id: int, avatar_img: String) -> void:
 	var firestore_collection: FirestoreCollection = Firebase.Firestore.collection(COLLECTION_NAME_PLAYERS)
-	print(firestore_collection, "collection in database manager")
 	var document = await firestore_collection.get_doc(user_id)
-	print(document, "document in database manager")
 	if document:
-		print("Updating document for user:", user_id)
 		document.add_or_update_field("username", username)
-		document.add_or_update_field("avatar_id", avatar_id)
+		print(username)
+		print(typeof(username))
+		document.add_or_update_field("avatar_id", str(avatar_id))
+		print(avatar_id)
+		print(typeof(avatar_id))
 		document.add_or_update_field("avatar_img", avatar_img)
-		#document.add_or_update_field("avatar_color", avatar_color)
+		print(avatar_img)
+		print(typeof(avatar_img))
 		var updated_document = await firestore_collection.update(document)
-		print("Updated document:", updated_document)
 	else:
 		print("Error: Document not found for user:", user_id)
 
 func get_player_data(user_id: String):
-	print("database manager Fetching player data for user:", user_id)
 	var firestore_collection: FirestoreCollection = Firebase.Firestore.collection(COLLECTION_NAME_PLAYERS)
 	var document = await firestore_collection.get_doc(user_id, false, true)
 	if document:
-		print("database manager Document fetched successfully:", document.document)
 		player_data_changed.emit(document.document)
 	else:
 		print("Error: Document not found for user:", user_id)
--- menus/user_profile.gd
+++ menus/user_profile.gd
@@ -1,41 +1,38 @@
-extends Control
+class_name UserProfile extends Control
 
 @onready var player_avatar = %PlayerAvatar
 
 func _ready():
-	print("User profile scene ready")
 	var auth = Firebase.Auth.auth
 	if auth.localid:
-		print("Local ID found in user profile:", auth.localid)
 		DatabaseManager.player_data_changed.connect(_on_player_data_changed)
 		DatabaseManager.get_player_data(auth.localid)
 	else:
 		print("No local ID found")
 			
 func _on_player_data_changed(data: Dictionary) -> void:
-	print("Fetched player data:", data)
 	if data.has("username"):
-		print("Setting username:", data["username"]["stringValue"])
 		%SnakeNameLabel.text = data["username"]["stringValue"]
 	if data.has("score"):
-		print("Setting score:", data["score"]["integerValue"])
 		%TotalScoreLabel.text = data["score"]["integerValue"]
 	if data.has("avatar_img"):
-		print("Loading avatar image from path:", data["avatar_img"]["stringValue"])
 		load_images_from_path(data["avatar_img"]["stringValue"])
 
 
 func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://menus/level_select.tscn")
+	get_tree().change_scene_to_file("res://menus/start_screen.tscn")
+	LevelsManager.get_levels_list()
+
 
 func load_images_from_path(path:String) -> void:
-	print("Fetching image from Firebase Storage at path:", path)
+	print(path)
 	var file_ref = Firebase.Storage.ref(path)
+	print(file_ref)
 	var data_task = await file_ref.get_data()
+	print(data_task)
 	_on_data_received(data_task)
 
 func _on_data_received(data: PackedByteArray):
-	print("Processing image data")
 	var image = Image.new()
 	image.load_png_from_buffer(data)
 	if image:
